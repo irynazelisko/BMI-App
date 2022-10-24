e37da2306256ddc9cc23a94eed4c5628e728a085
Iryna <irazelisko@gmail.com>
2022-10-24 13:14:09 +0300

    BMI App

---
+++ BMi App/Controllers/ResultsViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ResultsViewController.swift
+//  BMi App
+//
+//  Created by Ірина Зеліско on 24.10.2022.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    var bmiValue: String?
+    var advice: String?
+    var color: UIColor?
+    
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        view.backgroundColor = color
+       
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
---
+++ BMi App/Models/BMI.swift
@@ -0,0 +1,13 @@
+//
+//  BMI.swift
+//  BMi App
+//
+//  Created by Ірина Зеліско on 24.10.2022.
+//
+
+import UIKit
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
