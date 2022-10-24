b44c7e479a704e06e699888aae2c2f4601b25812
Iryna <irazelisko@gmail.com>
2022-10-24 15:05:38 +0300

    change text of advice

--- BMi App/Models/CalculatorBrain.swift
+++ BMi App/Models/CalculatorBrain.swift
@@ -14,11 +14,11 @@ struct CalculatorBrain {
         let bmiValue = weight / ( height * height)
         
         if bmiValue < 18.5 {
-            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1))
+            bmi = BMI(value: bmiValue, advice: "You are underweight", color: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1))
         }else if bmiValue < 24.5 {
-            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1))
+            bmi = BMI(value: bmiValue, advice: "You have a normal weight", color: #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1))
         }else {
-            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1) )
+            bmi = BMI(value: bmiValue, advice: "You are overweight", color: #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1) )
         }
     }
     
